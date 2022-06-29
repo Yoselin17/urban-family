1b18985cc0c0401b440183b82ee89a99c2ed4b5b
Yoselin17 <yoselin_yarg@hotmail.com>
2022-06-29 19:41:16 +0000

    family

---
+++ .venv/app.py
@@ -0,0 +1,53 @@
+members = jackson_family.get_all_members()
+response_body = {
+    "hello": "world",
+    "family": members
+}
+
+
+return jsonify(response_body),200
+
+
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
+
+#Todos
+@app.route('members', method =['GET'])
+def handle_hello():
+
+    menbers = jackson_family.get_all_members()
+    response_body = {
+        "hello": "Hola",
+        "family": members
+    }    
+
+    return jsonify(response_body), 200
+
+    #solo uno
+    @app.routes('member/<int:member_id', method = ['GET, DELETE'])
+    def one_nember(nember_id):
+       # member = jackson_family.query(member_id)
+       #  return jsonify(menber), 200
+       if request.method == "DELETE":
+        jackson_family.delete_member(member_id)
+
+
+    @app.route('/member', method = ['POST'])
+    def add_member():
+        print(request.json)
+        menber={
+            "id": jackson_family._generateId(),
+            "age": request.json.get("age"),
+            "first_name": request.json.get("first_name")
+            "last_name": request.json.get("last_name"),
+            "lucky_numbers": request.json.get("lucky_numbers"),      
+        }    
+
+        jackson_family.add_member(member)
+        return "se agrega un miembro"
+
+
+
+        crear funcion para a;adir los miembros POST
+        hacer funcion para eliminar los miembros DELETE
---
+++ .venv/datastructures.py
@@ -0,0 +1,69 @@
+class FamilyStructure:
+    def__init__(self, last_name):
+        self.id-id
+        self.first_name = ''
+        self.last_name = last_name
+        self.age = 0
+        self.lucky_numbers = []
+
+        # example list of members
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John"
+            "last_name": "Jackson",
+            "age": 33,
+            "lucky_numbers": [7,13,22]
+        },
+        {
+        "id": self._generateId(),
+            "first_name": "Jane"
+            "last_name": "Jackson",
+            "age": 35,
+            "lucky_numbers": [10,14,3]
+        },
+        {
+        "id": self._generateId(),
+            "first_name": "Jimy"
+            "last_name": "Jackson",
+            "age": 5,
+            "lucky_numbers": [1]
+        }    
+        ]    
+
+     # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
+        return randint(0, 99999999)
+
+    def add_member(self, member): 
+        self._members.append(member)
+        return "member""
+        pass
+
+    def delete_member(self, id):
+        for member in self._members:
+            if member.["id"] == id:
+                self._members.remove(member)
+                return "Deleted"
+
+                else:
+                    return "Not found"
+        pass
+
+    def update_member(self, id, member):
+        for member self member:
+            if member["id"] == id
+            return member
+        pass
+
+    def get_member(self, id):
+        traer los miembros segun su id
+        for member in self._members:
+            if member ["id"] == id:
+                return member
+                else:
+                    return "Not found"
+        pass
+
+    def get_all_members(self):
+        traer todos los miembros
+        return self._members    
